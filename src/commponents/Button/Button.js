cd4727b544054b9390d4b2af0b124ea77cf46366
Cipher-20 <kan20_2003@abv.bg>
2022-12-09 19:59:11 +0200

    Button component and print in output symbols

---
+++ src/commponents/Button/Button.js
@@ -0,0 +1,12 @@
+
+function Button({symbol, handleClick}) {
+
+    return(
+        <button
+        onClick={ () => handleClick(symbol)}>
+        {symbol}
+        </button>
+    )
+}
+
+export default Button;
--- src/commponents/CalcBody/CalcBody.js
+++ src/commponents/CalcBody/CalcBody.js
@@ -1,33 +1,46 @@
+import { useState } from "react";
+import Button from "../Button/Button";
 
 function CalcBody() {
+
+    const [text, setText] = useState("");
+    const [result, setResult] = useState(0);
+
+    const addText = (val) =>{
+        setText((text) => [...text, val + " "])
+    }
     
+    const handler = () => {
+        console.log(1);
+    }
+
     return(
     <>
 
         <div className="calculator-grid">
 
             <div className="output">
-                <div className="previous-operand">1111111</div>
-                <div className="currnet-operand">565656</div>
+                <div className="previous-operand">{text}</div>
+                <div className="currnet-operand">{result}</div>
             </div>
 
             <button className="span-two">AC</button>    
             <button>DEL</button>
-            <button>÷</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>*</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>-</button>
-            <button>.</button>
-            <button>0</button>
+            <Button symbol="÷" handleClick={addText}/>
+            <Button symbol="1" handleClick={addText}/>
+            <Button symbol="2" handleClick={addText}/>
+            <Button symbol="3" handleClick={addText}/>
+            <Button symbol="*" handleClick={addText}/>
+            <Button symbol="4" handleClick={addText}/>
+            <Button symbol="5" handleClick={addText}/>
+            <Button symbol="6" handleClick={addText}/>
+            <Button symbol="+" handleClick={addText}/>
+            <Button symbol="7" handleClick={addText}/>
+            <Button symbol="8" handleClick={addText}/>
+            <Button symbol="9" handleClick={addText}/>
+            <Button symbol="-" handleClick={addText}/>
+            <Button symbol="." handleClick={addText}/>
+            <Button symbol="0" handleClick={addText}/>
             <button className="span-two">=</button>
 
         </div>
