1ba0ccbd9bdf13b674eb5c199c444070c400d2fc
Cipher-20 <kan20_2003@abv.bg>
2022-12-13 23:51:04 +0200

    logic operators

--- src/commponents/CalcBody/CalcBody.js
+++ src/commponents/CalcBody/CalcBody.js
@@ -3,42 +3,115 @@ import Button from "../Button/Button";
 
 function CalcBody() {
 
-    const [text, setText] = useState("");
+    const [input, setInput] = useState("");
     const [result, setResult] = useState(0);
 
     const addText = (val) =>{
-        setText((text) => [...text, val + " "])
+        setInput((text) => [...text, val + ""])
     }
     
     const handler = () => {
         console.log(1);
     }
 
-    return(
-    <>
 
-        <div className="calculator-grid">
+    const deleteHandler = () =>{
 
-            <div className="output">
-                <div className="previous-operand">{text}</div>
-                <div className="currnet-operand">{result}</div>
-            </div>
+        // if (input.length > 1) {
 
-            <button className="span-two">AC</button>    
-            <button>DEL</button>
-            <Button symbol="÷" handleClick={addText}/>
-            <Button symbol="1" handleClick={addText}/>
-            <Button symbol="2" handleClick={addText}/>
-            <Button symbol="3" handleClick={addText}/>
-            <Button symbol="*" handleClick={addText}/>
+        //     const value = input.slice(0,-1);
+        //     setInput(value);
+
+        // }else{
+
+        //     const value = 0;
+        //     setInput(value);
+
+        // }
+
+
+        const value = input.slice(0,-1);
+        setInput(value);
+
+    }
+
+    const plusHandler = () =>{
+        let inputNumber = input.join('');
+        
+        let value = Number(result) + Number(inputNumber);
+
+        setResult(value);
+
+        setInput("")
+    }
+
+    const minusHandler = () =>{
+
+        let inputNumber = input.join('');
+         
+         let value = Number(result) - Number(inputNumber);
+ 
+         setResult(value);
+ 
+         setInput("")
+     }
+
+     const multiplyHandler = () =>{
+
+        let inputNumber = input.join('');
+         
+         let value = Number(result) * Number(inputNumber);
+ 
+         setResult(value);
+ 
+         setInput("")
+     }
+
+     const divisionHandler = () =>{
+        let inputNumber = input.join('');
+         
+        let value = Number(result) / Number(inputNumber);
+
+        setResult(value);
+
+        setInput("")
+    }
+
+    const acHandler = () =>{
+       setResult(0);
+
+       setInput("")
+    }
+
+
+
+
+
+   return(
+   <>
+
+       <div className="calculator-grid">
+
+           <div className="output">
+               <div className="previous-operand">{input}</div>
+               <div className="currnet-operand">{result}</div>
+           </div>
+
+           <button className="span-two" onClick={acHandler}>AC</button>    
+           <button onClick={deleteHandler}>DEL</button> 
+           <button onClick={divisionHandler}>÷</button>
+           <Button symbol="1" handleClick={addText}/>
+           <Button symbol="2" handleClick={addText}/>
+           <Button symbol="3" handleClick={addText}/>
+           <button onClick={multiplyHandler}>*</button>
             <Button symbol="4" handleClick={addText}/>
             <Button symbol="5" handleClick={addText}/>
             <Button symbol="6" handleClick={addText}/>
-            <Button symbol="+" handleClick={addText}/>
+            <button onClick={plusHandler}>+</button>
             <Button symbol="7" handleClick={addText}/>
             <Button symbol="8" handleClick={addText}/>
             <Button symbol="9" handleClick={addText}/>
-            <Button symbol="-" handleClick={addText}/>
+            <button onClick={minusHandler}>-</button>
             <Button symbol="." handleClick={addText}/>
             <Button symbol="0" handleClick={addText}/>
             <button className="span-two">=</button>
